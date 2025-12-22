0c2b99ae93554a09c0d9c5ada5c249d1108fb798
udayguntreddi90-dotcom <udayguntreddi90@gmail.com>
2025-12-22 15:15:37 +0530

    Create Task1.java

---
+++ Module1_Array/Task1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Task1 {
+    public static void main(String[] args) {
+       // Static D & I  
+        int[] n={1,2,3,4,5};
+        System.out.println(n[0]);
+        System.out.println(n.length);
+
+         for(int i=0;i<n.length;i++){
+            System.out.print(n[i]+" ");
+         }
+
+         // key word
+
+         int[] a= new int[] {1,2,3,4,5};
+            for(int i=0;i<a.length;i++){
+                System.out.print(a[i]+" ");
+            }
+             int[] b= new int[10];
+
+             b[0]=10;
+             b[1]=20;
+             for(int i=0;i<b.length;i++){
+                System.out.print(n2[i]+" ");
+             }}
